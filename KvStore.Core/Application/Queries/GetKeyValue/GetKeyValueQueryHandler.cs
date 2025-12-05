4e55fbb79ec9c419ea340ea1390930d28e42befb
Ikramul Haque Oli <olixbt@gmail.com>
2025-12-05 09:46:04 +0700

    Add logging in Query handler

--- KvStore.Core/Application/Queries/GetKeyValue/GetKeyValueQueryHandler.cs
+++ KvStore.Core/Application/Queries/GetKeyValue/GetKeyValueQueryHandler.cs
@@ -3,26 +3,41 @@ using KvStore.Core.Application.KeyValue.Responses;
 using KvStore.Core.Domain.Exceptions;
 using KvStore.Core.Domain.Repositories;
 using KvStore.Core.Domain.Validation;
+using Microsoft.Extensions.Logging;
 
 namespace KvStore.Core.Application.Queries.GetKeyValue;
 
 public sealed class GetKeyValueQueryHandler(
     IKeyValueRepository repository,
-    IKeyLockProvider keyLockProvider) : IQueryHandler<GetKeyValueQuery, KeyValueResponse>
+    IKeyLockProvider keyLockProvider,
+    ILogger<GetKeyValueQueryHandler> logger) : IQueryHandler<GetKeyValueQuery, KeyValueResponse>
 {
     public Task<KeyValueResponse> Handle(GetKeyValueQuery query, CancellationToken cancellationToken)
     {
         ArgumentNullException.ThrowIfNull(query);
         KeyValidator.EnsureValid(query.Key);
 
-        return keyLockProvider.ExecuteWithLockAsync(query.Key, async token =>
-        {
-            var aggregate = await repository.GetAsync(query.Key, token);
+        logger.LogInformation(
+            "Operation GET on key {Key} requested at {RequestedAt}",
+            query.Key,
+            DateTimeOffset.UtcNow);
 
-            return aggregate is null 
-            ? throw new KeyValueNotFoundException(query.Key)
-            : KeyValueResponse.FromSnapshot(aggregate.ToSnapshot());
-        }, cancellationToken);
+        return keyLockProvider.ExecuteWithLockAsync(query.Key, async token =>
+            await GetKeyValueAggregateAsync(query, token), cancellationToken);
+    }
+
+    private async Task<KeyValueResponse> GetKeyValueAggregateAsync(GetKeyValueQuery query, CancellationToken token)
+    {
+        logger.LogInformation(
+            "Operation GET on key {Key} starting at {ExecutionStart}",
+            query.Key,
+            DateTimeOffset.UtcNow);
+
+        var aggregate = await repository.GetAsync(query.Key, token);
+
+        return aggregate is null
+        ? throw new KeyValueNotFoundException(query.Key)
+        : KeyValueResponse.FromSnapshot(aggregate.ToSnapshot());
     }
 }
 
